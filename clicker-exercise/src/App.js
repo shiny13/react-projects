f589b2720bdb8894adcda11f2b777abb5701283c
Shahnawaz Alam <shahnawaz.alam13@gmail.com>
2020-02-19 11:48:12 +1100

    Clicker exercise to practice States in react

---
+++ clicker-exercise/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Clicker from './clicker';
+
+function App() {
+  return (
+    <div className="App">
+      <body>
+        <Clicker />
+      </body>
+    </div>
+  );
+}
+
+export default App;
