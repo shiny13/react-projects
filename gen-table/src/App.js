a7ffeb089ad5bf0b950e9b12ebaca886e81352c6
Shahnawaz Alam <shahnawaz.alam13@gmail.com>
2020-02-20 17:29:27 +1100

    Added gen-table project

---
+++ gen-table/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import InputValues from './InputValues';
+
+function App() {
+  return (
+    <div className="App">
+      <InputValues />
+    </div>
+  );
+}
+
+export default App;
