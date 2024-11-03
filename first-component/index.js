8807e6a7e53b6dc7d73038b297c3e6c8f8a96fe6
shiny13 <shahnawaz.alam13@gmail.com>
2024-11-03 15:01:34 +1100

    equivalent react function instead of class

--- first-component/index.js
+++ first-component/index.js
@@ -1,12 +1,21 @@
-class Hello extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>Hello there!</h1>
-                <p>This is a sample app to render a component.</p>
-            </div>
-        );
-    }
+// class Hello extends React.Component {
+//     render() {
+//         return (
+//             <div>
+//                 <h1>Hello there!</h1>
+//                 <p>This is a sample app to render a component.</p>
+//             </div>
+//         );
+//     }
+// }
+
+function Hello() {
+    return(
+        <div>
+           <h1>Hello there!</h1>
+           <p>This is a sample app to render a component.</p>
+        </div>
+    )
 }
 
 ReactDOM.render(<Hello />, document.getElementById('root'))
