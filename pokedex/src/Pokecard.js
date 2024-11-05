9631fb5e9c1adb21bf39e46b8a6c0f16116f8092
shiny13 <shahnawaz.alam13@gmail.com>
2024-11-05 21:48:29 +1100

    upgraded pokedex game

--- pokedex/src/Pokecard.js
+++ pokedex/src/Pokecard.js
@@ -2,17 +2,29 @@ import React, { Component } from 'react';
 import './Pokecard.css';
 const POKE_API = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/'
 
+const padToThree = (number) => {
+    if (number < 10) {
+        return `00${number}`;
+    } else if (number >- 10 && number < 100) {
+        return `0${number}`;
+    } else {
+        return `${number}`;
+    }
+};
+
 class Pokecard extends Component {
     render() {
-        let imgSrc = `${POKE_API}${this.props.id}.png`;
+        const {id, name, type, exp} = this.props;
+        console.log(padToThree(id));
+        let imgSrc = `${POKE_API}${padToThree(id)}.png`;
         return (
             <div className="Pokecard">
-                <h3 className="Pokecard-title">{this.props.name}</h3>
+                <h3 className="Pokecard-title">{name}</h3>
                 <div className="Pokecard-image">
-                    <img src={imgSrc} alt={this.props.name} />
+                    <img src={imgSrc} alt={name} />
                 </div>
-                <div className="Pokecard-data">Type: {this.props.type}</div>
-                <div className="Pokecard-data">EXP: {this.props.exp}</div>
+                <div className="Pokecard-data">Type: {type}</div>
+                <div className="Pokecard-data">EXP: {exp}</div>
             </div>
         )
     }
--- pokedex/src/Pokegame.js
+++ pokedex/src/Pokegame.js
@@ -4,14 +4,14 @@ import Pokedex from './Pokedex';
 class Pokegame extends Component {
     static defaultProps = {
         pokemon : [
-            {id: '004', name: 'Charmander', type: 'fire', base_experience: 62},
-            {id: '007', name: 'Squirtle', type: 'water', base_experience: 63},
-            {id: '011', name: 'Metapod', type: 'bug', base_experience: 72},
-            {id: '012', name: 'Butterfree', type: 'flying', base_experience: 178},
-            {id: '025', name: 'Pikachu', type: 'electric', base_experience: 112},
-            {id: '039', name: 'Jigglypuff', type: 'normal', base_experience: 95},
-            {id: '094', name: 'Gengar', type: 'poison', base_experience: 225},
-            {id: '133', name: 'Eevee', type: 'normal', base_experience: 65}
+            {id: 4, name: 'Charmander', type: 'fire', base_experience: 62},
+            {id: 7, name: 'Squirtle', type: 'water', base_experience: 63},
+            {id: 11, name: 'Metapod', type: 'bug', base_experience: 72},
+            {id: 12, name: 'Butterfree', type: 'flying', base_experience: 178},
+            {id: 25, name: 'Pikachu', type: 'electric', base_experience: 112},
+            {id: 39, name: 'Jigglypuff', type: 'normal', base_experience: 95},
+            {id: 94, name: 'Gengar', type: 'poison', base_experience: 225},
+            {id: 133, name: 'Eevee', type: 'normal', base_experience: 65}
         ]
     }
     render() {
